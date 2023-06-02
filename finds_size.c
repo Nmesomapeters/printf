1bb3732422e041b60b7f7a18372b5dc38ba96166
Nmesomapeters <105252598+Nmesomapeters@users.noreply.github.com>
2023-06-02 17:23:55 +0000

    Files

---
+++ finds_size.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_size - it calculates the size to cast the argument
+ * @format: Format
+ * @i: lists of arguments to print.
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_x = *i + 1;
+	int size = 0;
+
+	if (format[curr_x] == 'l')
+		size = S_LONG;
+	else if (format[curr_x] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_x - 1;
+	else
+		*i = curr_x;
+
+	return (size);
+}
---
+++ is_printable.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * is_printable - it evaluates if a char is printable
+ * @c: char to evaluate
+ * Return: 1 if c is printable, then 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: arrays of chars
+ * @i: index at which it to start append
+ * @ascii_code: ASCII CODE
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - it verifies if a char is a digit.
+ * @c: char to evaluate.
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - it casts a number to the specified size
+ * @num: number to cast.
+ * @size: number indicating the type to be cast.
+ * Return: cast value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - it casts a number to the specified size.
+ * @num: number to cast.
+ * @size: number indicating the type to cast
+ * Return: Cast value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
